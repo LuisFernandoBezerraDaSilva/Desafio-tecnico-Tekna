0f792adc4da8e4331783087a2322b6809a935c80
Luis Fernando <luisfernandobezerradasilva@hotmail.com>
2025-05-28 11:16:52 -0300

    route adjustments

--- back-end/routes/route.js
+++ back-end/routes/route.js
@@ -6,12 +6,12 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
-router.post('/auth/register', userController.create);
-router.post('/auth/login', authController.login);
+router.post('/auth/register', (req, res) => userController.create(req, res));
+router.post('/auth/login', (req, res) => authController.login(req, res));
 
-router.get('/tasks', authenticateToken, taskController.getAll);
-router.post('/tasks', authenticateToken, taskController.create);
-router.put('/tasks/:id', authenticateToken, taskController.update);
-router.delete('/tasks/:id', authenticateToken, taskController.delete);
+router.get('/tasks', authenticateToken, (req, res) => taskController.getAll(req, res));
+router.post('/tasks', authenticateToken, (req, res) => taskController.create(req, res));
+router.put('/tasks/:id', authenticateToken, (req, res) => taskController.update(req, res));
+router.delete('/tasks/:id', authenticateToken, (req, res) => taskController.delete(req, res));
 
 module.exports = router;

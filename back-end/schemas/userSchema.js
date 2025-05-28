16001128f5752c5f21f09958781a52f9d0d4dfcf
Luis Fernando <luisfernandobezerradasilva@hotmail.com>
2025-05-28 17:08:02 -0300

    removed user limitation & added stilization

--- back-end/schemas/userSchema.js
+++ back-end/schemas/userSchema.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
 const userSchema = Joi.object({
-  username: Joi.string().min(15).required(),
+  username: Joi.string().required(),
   password: Joi.string().required(),
   userId: Joi.string(),
 });

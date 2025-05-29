f9b918970ea64a8b42addc1948d9057e39098a1d
Luis Fernando <luisfernandobezerradasilva@hotmail.com>
2025-05-28 21:50:14 -0300

    created authErrorInterceptor

--- desafio-tekna/src/main.ts
+++ desafio-tekna/src/main.ts
@@ -3,12 +3,13 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { jwtInterceptor } from './app/interceptors/jwt.interceptor';
+import { AuthErrorInterceptor } from './app/interceptors/auth-error.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
     ...appConfig.providers,
     provideHttpClient(
-      withInterceptors([jwtInterceptor])
+      withInterceptors([jwtInterceptor, AuthErrorInterceptor])
     )
   ]
 });
